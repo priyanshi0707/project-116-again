ad7c7e22997ff3c84e083912d1f4ddbe44c78de7
priyanshi0707 <89638179+priyanshi0707@users.noreply.github.com>
2023-01-17 20:41:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+nose_x=0;
+nose_y=0;
+function preload() {
+    img=loadImage("https://i.postimg.cc/dt64qZtG/mustache.png");
+}
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    posenet=ml5.poseNet(video,modelloaded);
+    posenet.on("pose",gotposes);
+}
+function draw(){
+    image(video,0,0,300,300);
+    image(img nose,nose_x-15,nose_y-15
+          ,35,35);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+function modelloaded(){
+    console.log("posenet is initialized");
+}
+function gotposes(results){
+    if(results.length>0){
+        console.log(results);
+        console.log("nose x="+results[0].pose.nose.x);
+        console.log("nose y="+results[0].pose.nose.y);
+    }
+}
